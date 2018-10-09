5aebf133a1153ee369c0732432a593ab4f5a09e1
Lanmord <kostya.myndra@gmail.com>
2018-10-10 02:51:31 +0300

    Правки

--- main/script.js
+++ main/script.js
@@ -55,4 +55,35 @@ setInterval(function (){
     moveSlide(1);
 }, 5000);
 
+/////////// МОДАЛЬНОЕ ОКНО ////////////
 
+// Get modal element
+var modal = document.getElementById('ModalPanel');
+// Get open modal button
+var modalBtn = document.getElementById('feedback');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target == modal){
+    modal.style.display = 'none';
+  }
+}
