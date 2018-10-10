c00b21a86c3473b6ab463b80a0520349ab500b6c
Artur <elkin.as@mail.ru>
2018-10-10 23:17:31 +0300

    add two sliders and update main slider

---
+++ main/scripts/mainSlider.js
@@ -0,0 +1,69 @@
+// the main slider
+let slideIndex = 1,
+    wrap = document.querySelector('.slider_wrap'),
+    slides = document.getElementsByClassName('slider-item'),
+    prev = document.querySelector('.prev'),
+    next = document.querySelector('.next'),
+    dotsWrap = document.querySelector('.slider-dots'),
+    dots = document.getElementsByClassName('dot'),
+    intervalTime = 3300;
+
+function showSlides(index) {
+    if (index > slides.length) {
+        slideIndex = 1;
+    }
+    if (index < 1) {
+        slideIndex = slides.length;
+    }
+    for (let slide of slides) {
+        slide.style.display = 'none';
+    }
+    for (let dot of dots) {
+        dot.classList.remove('dot-active');
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('dot-active');
+}
+
+showSlides(slideIndex);
+
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+prev.addEventListener('click', () => {
+    moveSlide(-1);
+});
+
+next.addEventListener('click', () => {
+    moveSlide(1);
+});
+
+dotsWrap.addEventListener('click', function (event) {
+    for (let i = 0; i < dots.length + 1; ++i) {
+        if (event.target.classList.contains('dot') && event.target === dots[i - 1]) {
+            currentSlide(i);
+        }
+    }
+});
+
+let sliderInterval = setInterval(() => {
+    moveSlide(1)
+}, intervalTime);
+
+function onMouseOver() {
+    clearInterval(sliderInterval);
+}
+
+wrap.addEventListener('mouseover', onMouseOver);
+
+wrap.addEventListener('mouseleave', () => {
+    wrap.removeEventListener('mouseover', onMouseOver);
+    sliderInterval = setInterval(() => moveSlide(1), intervalTime);
+    wrap.addEventListener('mouseover', onMouseOver);
+});
