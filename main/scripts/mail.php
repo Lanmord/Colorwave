5ee0bd34a99efdb9c8f891d4a51d7b5945d7ed77
Lanmord <kostya.myndra@gmail.com>
2018-10-14 02:03:49 +0300

    правки

---
+++ main/scripts/mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$recepient = "kostya.myndra@yandex.by";
+$siteName = "Colorwave";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$message = "Имя: $name \nТелефон: $phone";
+
+$pagetitle = "Заявка с сайта \"$siteName\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
